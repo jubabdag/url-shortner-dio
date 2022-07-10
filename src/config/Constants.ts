e2425edcf1bb2ecf37aee7094b264ed043cefd4f
jubabdag <yoshikawa.juvenal@gmail.com>
2022-07-10 16:36:38 -0300

    Second commit

--- src/config/Constants.ts
+++ src/config/Constants.ts
@@ -1,4 +1,6 @@
+
 export const config = {
   API_URL: 'http://localhost:5000',
-  MONGO_CONNECTION: 'mongodb+srv://jubabdag2022:juba2022@url-shortener-dio.ingce.mongodb.net/?retryWrites=true&w=majority'
+  MONGO_CONNECTION: 'mongodb+srv://admin:admin123456@url-shortener-dio.ingce.mongodb.net/?retryWrites=true&w=majority'
+
 }
