db3156c61d7c2461cdb38d7d6caf4f776d0e9613
jubabdag <43036866+jubabdag@users.noreply.github.com>
2022-07-19 17:30:12 -0300

    Delete Constants.js

--- dist/config/Constants.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.config = void 0;
-exports.config = {
-    API_URL: 'http://localhost:5000',
-    MONGO_CONNECTION: 'mongodb+srv://admin:rtChnWPr6hG7ioQs@url-shortner-dio.vghpp.mongodb.net/?retryWrites=true&w=majority'
-};
-//# sourceMappingURL=Constants.js.map
